2a071c45057e2e44d69ccbc7afc52a6d22b6d4fa
13949836783@163.com <13949836783@163.com>
2017-07-09 21:01:15 +0800

    user can update temp

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,4 +4,15 @@ class CitiesController < ApplicationController
     @cities = City.all
   end
 
+  def update_temp
+    city = City.find(params[:id])
+    response = RestClient.get "http://v.juhe.cn/weather/index",
+                              params: {:cityname => city.juhe_id, :key => "5039b339a8ccb45cbd29f5bbe396b01c"}
+    data = JSON.parse(response.body)
+
+    city.update(:current_temp => data["result"]["sk"]["temp"])
+
+    redirect_to cities_path
+  end
+
 end
